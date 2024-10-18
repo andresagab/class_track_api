834f3dae7c73b0fdd922808c8c79f228e9f2bfd9
andresagab <andrescabj981@gmail.com>
2024-10-17 23:20:07 -0500

    Enable subjects router in main.py (app), and update .gitignore

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from config.database import Base, engine
 
 # add routers here ->
+from routers.subject_router import subject_router
 
 # create app
 app = FastAPI(debug=True)
 
 ### ROUTERS
+app.include_router(subject_router)
 
 ### META
 Base.metadata.create_all(bind=engine)
